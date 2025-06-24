f52024efb2054d0cc1ffe7c556840e768f481aca
Jonas Sundström <jonas@kirilla.se>
2025-06-24 21:09:08 +0200

    Discard cached log items directly, if not in production.

--- Application/Queues/LogItems/LogItemList.cs
+++ Application/Queues/LogItems/LogItemList.cs
@@ -1,11 +1,18 @@
-﻿namespace Betalish.Application.Queues.LogItems;
+﻿using Microsoft.Extensions.Hosting;
 
-public class LogItemList(IDateService dateService) : ILogItemList
+namespace Betalish.Application.Queues.LogItems;
+
+public class LogItemList(
+    IHostEnvironment hostEnvironment,
+    IDateService dateService) : ILogItemList
 {
     private List<LogItem> list { get; set; } = new List<LogItem>();
 
     public void AddLogItem(LogItem logItem)
     {
+        if (!hostEnvironment.IsProduction())
+            return;
+
         lock (this)
         {
             logItem.Created = dateService.GetDateTimeNow();
