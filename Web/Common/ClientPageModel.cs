c06590ff049b281c20eda6b552d37492803c0c61
Jonas Sundström <jonas@kirilla.se>
2025-06-29 23:26:19 +0200

    WIP: Harmonize the navbars.

--- Web/Common/ClientPageModel.cs
+++ Web/Common/ClientPageModel.cs
@@ -6,19 +6,6 @@ public class ClientPageModel(IUserToken userToken) : PageModel
 
     public Client Client { get; set; }
 
-    public ClientNavbarViewModel ClientNavbarViewModel => 
-        new ClientNavbarViewModel(UserToken, Client);
-
-    // UserToken comes from dependency injection,
-    // via the constructor.
-
-    // Client is populated by a database lookup
-    // in the page model subclass.
-
-    // The ClientNavbarViewModel getter is a convenience,
-    // meant to be passed as an argument for the
-    // _ClientNavbar partial view.
-
     public async Task AssertClientAuthorization(
         IDatabaseService database)
     {
