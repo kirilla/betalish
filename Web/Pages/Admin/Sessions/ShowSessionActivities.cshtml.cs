d0b9d593720a4240ec3680de199032b14f06eca2
Jonas Sundström <jonas@kirilla.se>
2025-06-30 20:01:05 +0200

    Add pages for session records and activities.

---
+++ Web/Pages/Admin/Sessions/ShowSessionActivities.cshtml.cs
@@ -0,0 +1,33 @@
+using Betalish.Persistence.Migrations;
+
+namespace Betalish.Web.Pages.Admin.Sessions;
+
+public class ShowSessionActivitiesModel(
+    IUserToken userToken,
+    IDatabaseService database) : AdminPageModel(userToken)
+{
+    public List<SessionActivity> SessionActivities { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        try
+        {
+            await AssertAdminAuthorization(database);
+
+            SessionActivities = await database.SessionActivities
+                .Include(x => x.Session.User)
+                .OrderByDescending(x => x.Created)
+                .ToListAsync();
+
+            return Page();
+        }
+        catch (NotFoundException)
+        {
+            return Redirect("/help/notfound");
+        }
+        catch
+        {
+            return Redirect("/help/notpermitted");
+        }
+    }
+}
---
+++ Web/Pages/Admin/Sessions/ShowSessionRecords.cshtml.cs
@@ -0,0 +1,32 @@
+namespace Betalish.Web.Pages.Admin.Sessions;
+
+public class ShowSessionRecordsModel(
+    IUserToken userToken,
+    IDatabaseService database) : AdminPageModel(userToken)
+{
+    public List<SessionRecord> SessionRecords { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        try
+        {
+            await AssertAdminAuthorization(database);
+
+            SessionRecords = await database.SessionRecords
+                .Include(x => x.User)
+                .OrderBy(x => x.Login)
+                .ThenBy(x => x.Logout)
+                .ToListAsync();
+
+            return Page();
+        }
+        catch (NotFoundException)
+        {
+            return Redirect("/help/notfound");
+        }
+        catch
+        {
+            return Redirect("/help/notpermitted");
+        }
+    }
+}
