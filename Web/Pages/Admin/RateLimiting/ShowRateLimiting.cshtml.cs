cd6501a6129c182f956b64538e27a15d379de5c9
Jonas Sundström <jonas@kirilla.se>
2025-07-06 17:54:35 +0200

    Add logs and endpoint rate limiting to the ShowRateLimiting page

--- Web/Pages/Admin/RateLimiting/ShowRateLimiting.cshtml.cs
+++ Web/Pages/Admin/RateLimiting/ShowRateLimiting.cshtml.cs
@@ -1,4 +1,5 @@
 using Betalish.Application.Models;
+using Betalish.Application.Queues.EndpointRateLimiting;
 using Betalish.Application.Queues.IpAddressRateLimiting;
 using Betalish.Application.Queues.SignInRateLimiting;
 
@@ -6,12 +7,16 @@ namespace Betalish.Web.Pages.Admin.RateLimiting;
 
 public class ShowRateLimitingModel(
     IUserToken userToken,
+    IEndpointRateLimiter endpointRateLimiter,
     IIpAddressRateLimiter ipAddressRateLimiter,
     ISignInRateLimiter signInRateLimiter,
     IDatabaseService database) : AdminPageModel(userToken)
 {
+    public List<LogItem> RateLimitLogItems { get; set; }
+
     public List<IpAddressEndpointHit> IpAddressEndpointHits { get; set; }
     public List<SignInAttempt> SignInAttempts { get; set; }
+    public List<EndpointHit> EndpointHits { get; set; }
 
     public async Task<IActionResult> OnGetAsync()
     {
@@ -19,12 +24,25 @@ public class ShowRateLimitingModel(
         {
             await AssertAdminAuthorization(database);
 
+            RateLimitLogItems = await database.LogItems
+                .Where(x =>
+                    x.LogItemKind == LogItemKind.EndpointRateLimited ||
+                    x.LogItemKind == LogItemKind.IpAddressRateLimited ||
+                    x.LogItemKind == LogItemKind.SignInRateLimited)
+                .OrderByDescending(x => x.Created)
+                .ToListAsync();
+
             IpAddressEndpointHits = ipAddressRateLimiter
                 .ToList()
                 .OrderBy(x => x.DateTime)
                 .ToList();
 
             SignInAttempts = signInRateLimiter
+                .ToList()
+                .OrderBy(x => x.DateTime)
+                .ToList();
+
+            EndpointHits = endpointRateLimiter
                 .ToList()
                 .OrderBy(x => x.DateTime)
                 .ToList();
