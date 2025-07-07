0f894fbe392de08e478d7bd1b3d83e7e810d6062
Jonas Sundström <jonas@kirilla.se>
2025-07-07 22:47:27 +0200

    Add a page to show log items.

---
+++ Web/Pages/Admin/LogItems/ShowLogItems.cshtml.cs
@@ -0,0 +1,27 @@
+namespace Betalish.Web.Pages.Admin.LogItems;
+
+public class ShowLogItemsModel(
+    IUserToken userToken,
+    IDatabaseService database) : AdminPageModel(userToken)
+{
+    public List<LogItem> LogItems { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        try
+        {
+            await AssertAdminAuthorization(database);
+
+            LogItems = await database.LogItems
+                .AsNoTracking()
+                .OrderByDescending(x => x.Created)
+                .ToListAsync();
+
+            return Page();
+        }
+        catch
+        {
+            return Redirect("/help/notpermitted");
+        }
+    }
+}
