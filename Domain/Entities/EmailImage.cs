8f35a1e657282c07a6383d832b0f2297f122ee9c
Jonas Sundström <jonas@kirilla.se>
2025-07-12 19:54:53 +0200

    Fixes.

--- Domain/Entities/EmailImage.cs
+++ Domain/Entities/EmailImage.cs
@@ -6,17 +6,12 @@ public class EmailImage
 {
     public int Id { get; set; }
 
-    public byte[] Data { get; set; }
+    public required byte[] Data { get; set; }
 
     public int ContentLength { get; set; }
 
-    [Required]
-    [StringLength(MaxLengths.Domain.EmailImage.Name)]
-    public string Name { get; set; }
-
-    [Required]
-    [StringLength(MaxLengths.Domain.EmailImage.ContentType)]
-    public string ContentType { get; set; }
+    public required string Name { get; set; }
+    public required string ContentType { get; set; }
 
     public int EmailMessageId { get; set; }
     public EmailMessage EmailMessage { get; set; } = null!;
