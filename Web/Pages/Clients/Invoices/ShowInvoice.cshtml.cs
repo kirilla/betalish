a874c2ad398efec6088c8d8d203c637c67931efa
Jonas Sundström <jonas@kirilla.se>
2025-07-21 23:58:12 +0200

    Tweaks.

---
+++ Web/Pages/Clients/Invoices/ShowInvoice.cshtml.cs
@@ -0,0 +1,47 @@
+﻿namespace Betalish.Web.Pages.Clients.Invoices;
+
+public class ShowInvoiceModel(
+    IUserToken userToken,
+    IDatabaseService database) : ClientPageModel(userToken)
+{
+    public Invoice Invoice { get; set; } = null!;
+
+    public List<InvoiceRow> InvoiceRows { get; set; } = [];
+
+    public async Task<IActionResult> OnGetAsync(int id)
+    {
+        try
+        {
+            if (!UserToken.IsAuthenticated)
+                throw new NotPermittedException();
+
+            AssertIsClient();
+
+            Invoice = await database.Invoices
+                .Where(x =>
+                    x.ClientId == UserToken.ClientId!.Value &&
+                    x.Id == id)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            InvoiceRows = await database.InvoiceRows
+                .AsNoTracking()
+                .Where(x =>
+                    x.Invoice.ClientId == UserToken.ClientId!.Value &&
+                    x.InvoiceId == id)
+                .OrderBy(x => x.ArticleNumber)
+                .ThenBy(x => x.ArticleName)
+                .ToListAsync();
+
+            return Page();
+        }
+        catch (NotFoundException)
+        {
+            return Redirect("/help/notfound");
+        }
+        catch
+        {
+            return Redirect("/help/notpermitted");
+        }
+    }
+}
