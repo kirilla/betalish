5b1e1b865cbcb9c8dff793e489d75e35d71bfba1
Jonas Sundström <jonas@kirilla.se>
2025-07-21 20:24:53 +0200

    Add null-ignoring unique indexing of invoice numbers.

--- Persistence/Configuration/InvoiceConfiguration.cs
+++ Persistence/Configuration/InvoiceConfiguration.cs
@@ -6,7 +6,10 @@ class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
     {
         builder.HasKey(p => p.Id);
 
-        builder.HasIndex(p => p.InvoiceNumber);
+        builder
+            .HasIndex(p => p.InvoiceNumber)
+           .IsUnique()
+           .HasFilter("[InvoiceNumber] IS NOT NULL");
 
         builder.Property(p => p.About)
             .HasMaxLength(MaxLengths.Common.Invoice.About);
---
+++ Persistence/Migrations/20250721182133_NullIgnoring_UniqueInvoiceNumbers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Betalish.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullIgnoring_UniqueInvoiceNumbers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_InvoiceNumber",
+                table: "Invoices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_InvoiceNumber",
+                table: "Invoices",
+                column: "InvoiceNumber",
+                unique: true,
+                filter: "[InvoiceNumber] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_InvoiceNumber",
+                table: "Invoices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_InvoiceNumber",
+                table: "Invoices",
+                column: "InvoiceNumber");
+        }
+    }
+}
