a07a411442486a12ec481b878bbb7d21a48e84e6
Jonas Sundström <jonas@kirilla.se>
2025-07-21 23:58:02 +0200

    Make it more like the similar decimal extension method.

--- Common/Extensions/NumberExtensions.cs
+++ Common/Extensions/NumberExtensions.cs
@@ -1,9 +1,11 @@
-﻿namespace Betalish.Common.Extensions;
+﻿using Betalish.Common.Locale;
+
+namespace Betalish.Common.Extensions;
 
 public static class NumberExtensions
 {
-    public static string ToSwedishSpacing(this int number)
+    public static string ToSwedish(this int number)
     {
-        return number.ToString("N0", new CultureInfo("sv-SE"));
+        return number.ToString("N0", Swedish.CultureInfo);
     }
 }
