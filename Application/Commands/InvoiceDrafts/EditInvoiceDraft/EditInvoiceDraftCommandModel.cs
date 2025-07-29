46f6e81c5793ccce27574d690099a19d6c5460f5
Jonas Sundström <jonas@kirilla.se>
2025-07-29 06:28:27 +0200

    Drop the required keyword.
    To make the target-typed new() work in the razor page.

--- Application/Commands/InvoiceDrafts/EditInvoiceDraft/EditInvoiceDraftCommandModel.cs
+++ Application/Commands/InvoiceDrafts/EditInvoiceDraft/EditInvoiceDraftCommandModel.cs
@@ -13,48 +13,48 @@ public class EditInvoiceDraftCommandModel
     public string? About { get; set; }
 
     // Dates
-    public required string? InvoiceDate { get; set; }
+    public string? InvoiceDate { get; set; }
 
     // Terms
-    public required int? PaymentTermDays { get; set; }
+    public int? PaymentTermDays { get; set; }
 
     [StringLength(
         MaxLengths.Common.Invoice.PaymentTerms,
         ErrorMessage = "Skriv kortare.")]
-    public required string? PaymentTerms { get; set; }
+    public string? PaymentTerms { get; set; }
 
     // Customer address
     [StringLength(
         MaxLengths.Common.Address.Address1,
         ErrorMessage = "Skriv kortare.")]
-    public required string? Customer_Address1 { get; set; }
+    public string? Customer_Address1 { get; set; }
 
     [StringLength(
         MaxLengths.Common.Address.Address2,
         ErrorMessage = "Skriv kortare.")]
-    public required string? Customer_Address2 { get; set; }
+    public string? Customer_Address2 { get; set; }
 
     [Required(ErrorMessage = "Ange postnummer.")]
     [StringLength(
         MaxLengths.Common.Address.ZipCode,
         ErrorMessage = "Skriv kortare.")]
-    public required string Customer_ZipCode { get; set; }
+    public string Customer_ZipCode { get; set; }
 
     [Required(ErrorMessage = "Ange postort.")]
     [StringLength(
         MaxLengths.Common.Address.City,
         ErrorMessage = "Skriv kortare.")]
-    public required string Customer_City { get; set; }
+    public string Customer_City { get; set; }
 
     [StringLength(
         MaxLengths.Common.Address.Country,
         ErrorMessage = "Skriv kortare.")]
-    public required string? Customer_Country { get; set; }
+    public string? Customer_Country { get; set; }
 
     // Customer email
     [RegularExpression(Pattern.Common.Email.Address)]
     [StringLength(
         MaxLengths.Common.Email.Address,
         ErrorMessage = "Skriv kortare.")]
-    public required string? Customer_Email { get; set; }
+    public string? Customer_Email { get; set; }
 }
