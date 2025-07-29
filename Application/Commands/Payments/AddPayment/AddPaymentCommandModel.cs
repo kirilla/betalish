744751b91771c453d4c4e051a4e1590493272a3b
Jonas Sundström <jonas@kirilla.se>
2025-07-29 13:32:03 +0200

    Reorder.

--- Application/Commands/Payments/AddPayment/AddPaymentCommandModel.cs
+++ Application/Commands/Payments/AddPayment/AddPaymentCommandModel.cs
@@ -5,20 +5,20 @@ namespace Betalish.Application.Commands.Payments.AddPayment;
 public class AddPaymentCommandModel
 {
     [RegularExpression(
-        Pattern.Common.Decimal.Signed.TwoOptionalDecimals,
-        ErrorMessage = "Belopp anges med två decimaler.")]
-    [Required(ErrorMessage = "Ange belopp.")]
-    public string? Amount { get; set; }
-
-    [RegularExpression(
         Pattern.Common.Date.Iso8601Permissive,
         ErrorMessage = "Ange datum i formatet ÅÅÅÅ-MM-DD.")]
     [Required(ErrorMessage = "Ange datum.")]
     public string? Date { get; set; }
 
-    [Required(ErrorMessage = "Ange betalsätt.")]
-    public PaymentMethod? PaymentMethod { get; set; }
+    [RegularExpression(
+        Pattern.Common.Decimal.Signed.TwoOptionalDecimals,
+        ErrorMessage = "Belopp anges med två decimaler.")]
+    [Required(ErrorMessage = "Ange belopp.")]
+    public string? Amount { get; set; }
 
     [Required(ErrorMessage = "Ange konto.")]
     public int? PaymentAccountId { get; set; }
+
+    [Required(ErrorMessage = "Ange betalsätt.")]
+    public PaymentMethod? PaymentMethod { get; set; }
 }
--- Application/Commands/Payments/EditPayment/EditPaymentCommandModel.cs
+++ Application/Commands/Payments/EditPayment/EditPaymentCommandModel.cs
@@ -7,20 +7,20 @@ public class EditPaymentCommandModel
     public int Id { get; set; }
 
     [RegularExpression(
-        Pattern.Common.Decimal.Signed.TwoOptionalDecimals,
-        ErrorMessage = "Belopp anges med två decimaler.")]
-    [Required(ErrorMessage = "Ange belopp.")]
-    public string? Amount { get; set; }
-
-    [RegularExpression(
         Pattern.Common.Date.Iso8601Permissive,
         ErrorMessage = "Ange datum i formatet ÅÅÅÅ-MM-DD.")]
     [Required(ErrorMessage = "Ange datum.")]
     public string? Date { get; set; }
 
-    [Required(ErrorMessage = "Ange betalsätt.")]
-    public PaymentMethod? PaymentMethod { get; set; }
+    [RegularExpression(
+        Pattern.Common.Decimal.Signed.TwoOptionalDecimals,
+        ErrorMessage = "Belopp anges med två decimaler.")]
+    [Required(ErrorMessage = "Ange belopp.")]
+    public string? Amount { get; set; }
 
     [Required(ErrorMessage = "Ange konto.")]
     public int? PaymentAccountId { get; set; }
+
+    [Required(ErrorMessage = "Ange betalsätt.")]
+    public PaymentMethod? PaymentMethod { get; set; }
 }
