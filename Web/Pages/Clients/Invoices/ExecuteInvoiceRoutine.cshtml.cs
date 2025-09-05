e91d6379385e7d4ad27e7b721f87469717c4f014
Jonas Sundström <jonas@kirilla.se>
2025-09-05 14:37:12 +0200

    Nit.

--- Web/Pages/Clients/Invoices/ExecuteInvoiceRoutine.cshtml.cs
+++ Web/Pages/Clients/Invoices/ExecuteInvoiceRoutine.cshtml.cs
@@ -64,7 +64,7 @@ public class ExecuteInvoiceRoutineModel(
 
             return Redirect($"/show-invoice/{id}");
         }
-        catch (Exception ex)
+        catch
         {
             return Redirect("/help/notpermitted");
         }
